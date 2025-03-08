d1d8bdc6c8f0cfaa341c9a936dd44e1b0f9ecd30
Kalicz Péter <kaliczp@gmail.com>
2025-03-08 18:01:12 +0100

    Read in all from 2005

--- Temper.R
+++ Temper.R
@@ -2,6 +2,15 @@ Sys.setenv(TZ='UTC')
 library(xts)
 ### h1
 ## Load necessary dates
-attach("../2010/h1/.RData")
+attach("../2005/h1/.RData")
 temp.h1 <- hom
 detach(pos = 2)
+
+for(ttev in 2006:2025) {
+    attach(paste0("../",ttev,"/h1/.RData"))
+    ttaktind <- index(temp.h1)
+    ttaktend <- ttaktind[length(ttaktind)]
+    ttaktstart <- ttaktend + 30*60
+    temp.h1 <- c(temp.h1, hom[paste(ttaktstart,"/")])
+    detach(pos = 2)
+}
